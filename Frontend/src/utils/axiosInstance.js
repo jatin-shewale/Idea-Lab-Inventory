ed2ba7452ec35ef37c3066ef00bec1ea3473aa83
jatin-shewale <shewalejatin427@gmail.com>
2025-04-04 07:19:39 +0530

    Update frontend configuration for deployment: Add .gitignore and update axiosInstance with environment variable

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080', // Replace with your backend URL
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
